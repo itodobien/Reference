ddc8a181e7b566f896d330eb3cfb6fae681a2b26
itodobien <itodobien@gmail.com>
2023-04-26 06:12:09 -1000

    Cleaned up spacing in VACompensationRates

--- Reference/MainPage.xaml.cs
+++ Reference/MainPage.xaml.cs
@@ -14,6 +14,7 @@ namespace Reference
 
             ParentsPicker.ItemsSource = new List<int> { 0, 1, 2 };
             ChildrenPicker.ItemsSource = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ChildrenPicker18.ItemsSource = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         }
 
         private void OnMarriedToggled(object sender, ToggledEventArgs e)
